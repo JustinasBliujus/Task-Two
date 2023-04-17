f72816e4e29865773d1e0357f4f69ba0f71ee97e
JustinasBliujus <45039478+JustinasBliujus@users.noreply.github.com>
2023-04-17 16:51:32 +0300

    Create analize.cpp

---
+++ Deque2/analize.cpp
@@ -0,0 +1,148 @@
+#include "antrastes.h"
+
+bool compare_by_baigiamasis(const studentas &a, const studentas &b)
+{
+    return a.baigiamasis < b.baigiamasis;
+}
+void analize(deque<studentas> &grupe, int kiek)
+{
+    for (int i = 0; i < kiek; i++)
+    {
+        char tipas;
+        string failas;
+        cout << "Failas bus generuojamas (g) ar naudojamas jau sukurtas (s)?" << endl;
+        while (true)
+        {
+            if (!(cin >> tipas) || (tipas != 'g' && tipas != 's'))
+            {
+                cout << "Netinkamas pasirinkimo tipas." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (tipas == 'g')
+        {
+            failas = generator();
+        }
+        else if (tipas == 's')
+        {
+            ifstream input_file;
+            cout << "Iveskite failo pavadinima: " << endl;
+            do
+            {
+                try
+                {
+                    cin >> failas;
+                    input_file.open(failas);
+                    if (!input_file)
+                    {
+                        string error = "Tokio failo nera";
+                        throw error;
+                    }
+                    else
+                    {
+                        input_file.close();
+                        break;
+                    }
+                }
+                catch (string error)
+                {
+                    cout << error << endl;
+                }
+            } while (true);
+        }
+        auto start = high_resolution_clock::now();
+        duration<double> total_diff(0);
+        Is_Failo(grupe, failas);
+
+        auto end = high_resolution_clock::now();
+        duration<double> diff = end - start;
+        cout << "Failo " << failas << " nuskaitymas i vektoriu uztruko:  " << diff.count() << " s\n";
+        total_diff += diff;
+
+        start = high_resolution_clock::now();
+        for (int i = 0; i < grupe.size(); i++)
+        {
+            grupe[i].baigiamasis = galutinis(grupe[i]);
+        }
+        std::sort(grupe.begin(), grupe.end(), compare_by_baigiamasis);
+
+        deque<studentas> levekai;
+
+        auto it = std::remove_if(grupe.begin(), grupe.end(),
+                                 [&](const studentas &student)
+                                 {
+                                     if (student.baigiamasis < 5)
+                                     {
+                                         levekai.push_back(student);
+                                         return true;
+                                     }
+                                     return false;
+                                 });
+
+        grupe.erase(it, grupe.end());
+
+        end = high_resolution_clock::now();
+        diff = end - start;
+        cout << "Vektoriaus padalijimas i dvi dalis uztruko:  " << diff.count() << " s\n";
+        total_diff += diff;
+        start = high_resolution_clock::now();
+
+        string file_name = "levekai" + std::to_string(i) + ".txt";
+        std::ofstream file(file_name);
+
+        for (int i = 0; i < levekai.size(); i++)
+        {
+            file << setw(15) << levekai[i].vardas << setw(15) << levekai[i].pavarde;
+            for (int j = 0; j < levekai[i].paz.size(); j++)
+            {
+                file << setw(5) << levekai[i].paz[j] << " ";
+            }
+            file << setw(7) << levekai[i].egz;
+            file << setw(10) << setprecision(2) << fixed << levekai[i].baigiamasis << endl;
+        }
+
+        file.close();
+
+        end = high_resolution_clock::now();
+        diff = end - start;
+        cout << "Failo " << file_name << " isvedimas uztruko: " << diff.count() << " s\n";
+        file_name = "ketekai" + std::to_string(i) + ".txt";
+        std::ofstream anotherfile(file_name);
+        total_diff += diff;
+        start = high_resolution_clock::now();
+        for (int k = 0; k < grupe.size(); k++)
+        {
+            anotherfile << setw(15) << grupe[k].vardas << setw(15) << grupe[k].pavarde;
+            for (int l = 0; l < grupe[k].paz.size(); l++)
+            {
+                anotherfile << setw(5) << grupe[k].paz[l] << " ";
+            }
+            anotherfile << setw(7) << grupe[k].egz;
+            anotherfile << setw(10) << setprecision(2) << fixed << grupe[k].baigiamasis << endl;
+        }
+
+        anotherfile.close();
+
+        end = high_resolution_clock::now();
+        diff = end - start;
+        cout << "Failo " << file_name << " isvedimas uztruko: " << diff.count() << " s\n";
+        total_diff += diff;
+        cout << "Bendrai darbas su failu '" << failas << "' uztruko: " << total_diff.count() << " s\n";
+
+        for (auto it = grupe.begin(); it != grupe.end(); ++it)
+        {
+            it->paz.clear();
+        }
+        grupe.clear();
+        for (auto it = levekai.begin(); it != levekai.end(); ++it)
+        {
+            it->paz.clear();
+        }
+        levekai.clear();
+    }
+}
